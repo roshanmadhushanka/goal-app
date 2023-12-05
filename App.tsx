b32c237f75ef8375993eaf390955a99906201fd1
Roshan Alwis <alwisroshan.13@cse.mrt.ac.lk>
2023-12-05 01:35:10 -0600

    Configure redux to add goal

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {Provider} from "react-redux";
 import { StyleSheet, View } from 'react-native';
 import 'react-native-get-random-values';
-import GoalList from './app/component/goalList.component';
-import AddGoal from './app/component/addItem.component';
+import GoalList from './component/goalList.component';
+import AddGoal from './component/addItem.component';
+import {store} from "./redux/store";
 
 const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <AddGoal/>
-      <GoalList/>
-    </View>
+      <Provider store={store}>
+          <View style={styles.container}>
+              <AddGoal/>
+              <GoalList/>
+          </View>
+      </Provider>
   )
 }
 
